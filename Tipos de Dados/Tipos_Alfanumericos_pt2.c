4015646e67317c95e7e3d35a5daee23ca09437b3
Giliard P. Santos <gil2614@live.com>
2022-06-16 12:48:24 -0300

    Tipos Alfanuméricos - pt2

---
+++ Tipos de Dados/Tipos_Alfanumericos_pt2.c
@@ -0,0 +1,27 @@
+/*
+ * Tipos de Dados - Alfanuméricos
+ * 		- Characteres;
+ * 		- Strings*;
+ *
+ * 	Na linguagem C, não existe o tipo de dado String.
+ *
+ * 	Ex; String => "asdfghert" tudo que estiver dentro de aspas duplas
+ *
+ * 	Ex caractere => 's'  tudo que estiver dentro de aspas simples
+ */
+
+#include <stdio.h>
+
+int main(){
+	// Declarando variavel em C com 50 caracteres
+	char nome[50];
+
+	// Saída de Dados
+	printf("Qual é o seu nome? \n");
+	// Para Receber uma string via teclado, utilizamos o gets
+	gets (nome);
+	// Usamos %s para dizer que o dado recebido será do tipo STRING
+	printf("Seu nome é %s", nome);
+
+	return 0;
+}
