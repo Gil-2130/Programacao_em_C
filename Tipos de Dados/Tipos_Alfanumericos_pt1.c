36fed0f685ba88efbe864b5e02a115dbb0b5434a
Giliard P. Santos <gil2614@live.com>
2022-06-16 12:47:46 -0300

    Tipos Alfanuméricos - pt1

---
+++ Tipos de Dados/Tipos_Alfanumericos_pt1.c
@@ -0,0 +1,50 @@
+/*
+ * Tipos de Dados - Alfanuméricos
+ * 		- Characteres;
+ * 		- Strings*;
+ *
+ * 	Na linguagem C, não existe o tipo de dado String.
+ *
+ * 	Ex; String => "asdfghert" tudo que estiver dentro de aspas duplas
+ *
+ * 	Ex caractere => 's'  tudo que estiver dentro de aspas simples
+ */
+
+#include <stdio.h>
+
+int main(){
+	// Declarando variavel do tipo caractere
+	char opcao;
+
+	// Saída de dados
+	printf("Informe a opção: \n");
+	printf("A - Saldo da conta. \n");
+	printf("B - Extrato da conta. \n");
+	printf("C - Limite da conta. \n");
+
+	// Entrada de dados
+	scanf("%c", &opcao);
+
+	// Condicional if
+	if(opcao == 'A'){
+		printf("Seu saldo é ...\n");
+	}
+	else if(opcao == 'B'){
+		printf("Extrato da conta:...\n");
+	}
+	else if(opcao == 'C'){
+		printf("Limite da conta:...\n");
+	}
+	else{
+		printf("Opção inválida...\n");
+	}
+
+	// Gerando alfabeto através de código alfanumérico da tabela ASCII
+	for(int i = 97; i <= 122 ; i +=1){
+		printf("Dicionário Alfanumérico: \n");
+		printf("Imprimindo em Decimal; %d\n", i);
+		printf("Imprimindo em Caractere; %c\n", i);
+	}
+
+	return 0;
+}
