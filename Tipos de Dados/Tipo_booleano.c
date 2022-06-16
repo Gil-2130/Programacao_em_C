917301fa4834868b70241f6764d585502c0de1ea
Giliard P. Santos <gil2614@live.com>
2022-06-16 12:48:53 -0300

    Tipo Booleano

---
+++ Tipos de Dados/Tipo_booleano.c
@@ -0,0 +1,27 @@
+/*
+ * Tipos de Dados - Booleanos => Verdadeiro/True ou Falso/False
+ *
+ * Na linguagem C, não existe o tipo de dado boolean
+ *
+ * mas...
+ *
+ * A linguagem C, reconhece o valor 0 como False
+ * e qualquer valor diferente de 0 como verdadeiro.
+ */
+
+#include <stdio.h>
+
+int main(){
+	// Declarando variável do tipo inteiro
+	int boolean = 1;
+
+	// Condicional
+	if(boolean){
+		printf("Verdadeiro");
+	}
+	else{
+		printf("Falso");
+	}
+
+	return 0;
+}
