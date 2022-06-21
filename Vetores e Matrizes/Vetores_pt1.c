0803eb6d1e046e96ff0d92275dfd2ac4b9b4853b
Giliard P. Santos <gil2614@live.com>
2022-06-21 12:25:32 -0300

    Vetores pt1

---
+++ Vetores e Matrizes/Vetores_pt1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+	// Vetores e Strings
+	char nome[50];
+	printf("Qual é o seu nome? ");
+	fflush(stdout);
+	gets(nome);
+	printf("Olá %s\n", nome);
+
+	// Vetores e Caracteres
+	char letras[26];
+	// Criando contador
+	int cont = 0;
+	// Criando laço(para criar alfabeto ASCII)
+	for(int i = 97; i <=122; i++){
+		letras[cont] = i;
+		cont= cont + 1;
+	}
+	// Imprimindo o Alfabeto e seus valores decimais
+	for(int i = 0; i < 26; i++){
+		printf("%d = %c\n", letras[i], letras[i]);
+	}
+	return 0;
+}
