6ae50c801ac2ee038c23fb72a6afaa85dd61969a
Giliard P. Santos <gil2614@live.com>
2022-06-21 12:26:04 -0300

    Vetores pt2

---
+++ Vetores e Matrizes/Vetores_pt2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+
+	// Vetores de Inteiros
+	int numeros[10];
+	numeros[0] = 1;
+	numeros[1] = 3;
+	numeros[2] = 5;
+	numeros[3] = 7;
+	numeros[4] = 9;
+
+	// Vetores e reais
+	float valores[5];
+	for(int i = 0; i< 5; i++){
+		valores[i] = numeros[i] / 2;
+	}
+	for(int i = 4; i >= 0; i--){
+		printf("%.2f\n", valores[i]);
+	}
+
+	return 0;
+}
