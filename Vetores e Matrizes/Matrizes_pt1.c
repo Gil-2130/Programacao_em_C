33615f8012f0e1822948eea739aa60b6e9a0436b
Giliard P. Santos <gil2614@live.com>
2022-06-21 12:26:39 -0300

    Matrizes pt1

---
+++ Vetores e Matrizes/Matrizes_pt1.c
@@ -0,0 +1,32 @@
+/*
+ * Matrizes Pt2
+ *
+ * array Uni-Dimensional (vetor)
+ * [0][1][2][3][4]
+ *
+ * array Multi-Dimensional (Matriz)
+ * [00][01][02][03][04]
+ * [10][11][12][13][14]
+ * [20][21][22][23][24]
+ * [30][31][32][33][34]
+ * [40][41][42][43][44]
+ *
+ */
+
+#include <stdio.h>
+
+int main(){
+	// vetores
+	char nome[3][50];
+
+	for(int i = 0; i < 3; i++){
+		printf("Qual é o seu nome? \n");
+		fflush(stdout);
+		gets(nome[i]);
+	}
+
+	for(int i = 0; i < 3; i++){
+		printf("Olá %s\n", nome[i]);
+	}
+	return 0;
+}
