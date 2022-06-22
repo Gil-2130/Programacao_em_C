99e3fe0f212c8e01494e2cb738ceecda8eb83bf4
Giliard P. Santos <gil2614@live.com>
2022-06-22 13:19:54 -0300

    Add files via upload

---
+++ Estruturas Condicionais/if-else.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+	int idade;
+	printf("Qual é a sua idade?\n");
+	scanf("%d", &idade);
+
+	if(idade <= 18){
+		printf("você é jovem\n");
+	}
+	else if(idade > 18 && idade < 50){
+		printf("você é adulto\n");
+	}
+	else{
+		printf("Você é idoso\n");
+	}
+
+	printf("\nSua idade é %d", idade);
+
+	return 0;
+}
