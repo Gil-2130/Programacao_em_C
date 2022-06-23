f7e44b31790375dacbb38fa62953caed456def1f
Giliard P. Santos <gil2614@live.com>
2022-06-23 10:07:23 -0300

    Arquivo de Funções para o programa

---
+++ Funcoes_em_C/ajuda.c
@@ -0,0 +1,13 @@
+// Criando funções
+void mensagem(){
+	// Declaração implícita do printf
+	printf("Olá...\n");
+}
+
+int soma(int n1, int n2){
+	return n1 + n2;
+}
+
+int mult(int n1, int n2){
+	return n1 * n2;
+}
