78db153f1e6f40c227734d7db58728bfb0743c29
Giliard P. Santos <gil2614@live.com>
2022-06-23 10:04:27 -0300

    Escrevendo Funções

---
+++ Funcoes_em_C/Escrevendo_Funcoes.c
@@ -0,0 +1,36 @@
+/*
+ * Escrevendo funções
+ *
+ * Tipo de retorno;
+ * nome;
+ * parâmetros de entrada (opcional);
+ * implementação;
+ * retorno (opcional)
+ */
+
+// Importando biblioteca de entrada e saída padrão
+#include <stdio.h>
+
+// função vazia (sem retorno)
+void mensagem(){
+	printf("Olá Mundo");
+}
+
+// Função de soma
+int soma(int num1, int num2){
+	return num1 + num2;
+	// ou
+//  int res = num1 + num2;
+//	return res;
+}
+
+void proximo_char(char caractere){
+	printf("%c", caractere + 1);
+}
+
+//Programa principal(este será executado primeiro antes dos outros)
+int main(){
+	printf("Olá...");
+
+	return 0; // Significa executado com sucesso. Qualquer valor diferente disso, significa foi executado sem sucesso.
+}
