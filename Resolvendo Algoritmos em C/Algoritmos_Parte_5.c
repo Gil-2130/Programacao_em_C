490a15ac96aeb8ae2f142e6e59278ef4b7a1b08f
Giliard P. Santos <gil2614@live.com>
2022-06-24 10:41:57 -0300

    Algoritmos Parte 5

---
+++ Resolvendo Algoritmos em C/Algoritmos_Parte_5.c
@@ -0,0 +1,36 @@
+/*
+ * faça um algoritmo que pergunte quanto você ganha por hora e
+ * o número de horas trabalhadas no mês.
+ * Calcule e mostre o total do seu salário no referido mês.
+ *
+ * CALCULA SALÁRIO
+ * A) Receba a quantidade de horas trabalhadas
+ * B) Receba o valor da hora
+ * C) Multiplique o número de horas trabalhadas pelo valor da hora
+ * D) Mostre o resultado da operação
+ */
+
+#include <stdio.h>
+
+int salario(int n1, int n2){
+	return (n1 * n2);
+}
+
+int main(){
+
+	int horas, valor_hora, pagamento;
+
+	printf("Qual a quantidade de horas trabalhadas? \n");
+	fflush(stdout);
+	scanf("%d", &horas);
+
+	printf("Qual o valor por hora trabalhada? \n");
+	fflush(stdout);
+	scanf("%d", &valor_hora);
+
+	pagamento = salario(horas, valor_hora);
+
+	printf("Seu salário por mês será de %dR$", pagamento);
+
+	return 0;
+}
