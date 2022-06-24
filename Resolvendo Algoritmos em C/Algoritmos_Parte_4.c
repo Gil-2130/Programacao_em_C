8536767c831a2e2a9f0523af57a63b15a84bf9d7
Giliard P. Santos <gil2614@live.com>
2022-06-24 10:41:23 -0300

    Algoritmos Parte 4

---
+++ Resolvendo Algoritmos em C/Algoritmos_Parte_4.c
@@ -0,0 +1,31 @@
+/*
+ * Faça um algoritmo que converta metros para centímetros.
+ *
+ * CONVERSÃO:
+ *
+ * A) Receba o tamanho em metros
+ * B) Multiplique o tamanho por 100
+ * C) Imprima o resultado da multiplicação
+ */
+
+// Biblioteca de entrada e saída de dados
+#include <stdio.h>
+
+// Criando programa
+int main(){
+	// Declarando variáveis
+	float metro, centimetro;
+
+	// Entrada de Dados
+	printf("Insira a medida em metros: ");
+	fflush(stdout);
+	scanf("%f", &metro);
+
+	// Processamento
+	centimetro = metro * 100;
+
+	// Saída
+	printf("O valor inserido, corresponde a %.2f centímetros", centimetro);
+
+	return 0;
+}
