44f7e731a2205772a0f1097c32936f4bdabb548b
Giliard P. Santos <gil2614@live.com>
2022-06-24 10:40:54 -0300

    Algoritmos Parte 3

---
+++ Resolvendo Algoritmos em C/Algoritmos_Parte_3.c
@@ -0,0 +1,32 @@
+/*
+ * Faça um algoritmo que peça dois números e imprima a soma deles
+ *
+ * SOMAR:
+ * A) Receba o 1º número
+ * B) Receba o 2º número
+ * C) Some os números recebidos
+ * D) Mostre o resultado da soma anterior
+ */
+
+#include <stdio.h>
+
+int main(){
+	// Declarando variáveis
+	int num1, num2, soma;
+
+	// Entradas
+	printf("Informe o 1º número: ");
+	fflush(stdout);
+	scanf("%d", &num1);
+
+	printf("Informe o 2º número: ");
+	fflush(stdout);
+	scanf("%d", &num2);
+
+	// Processamento
+	soma = num1 + num2;
+
+	// Saída
+	printf("A soma de %d + %d = %d", num1, num2, soma);
+	return 0;
+}
