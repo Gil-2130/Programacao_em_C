9b7221256cb6cc9cafd3f1c0645ec7de4c0355cf
Giliard P. Santos <gil2614@live.com>
2022-06-26 20:08:36 -0300

    Ponteiros pt2

--- Ponteiros em C/Sobre Ponteiros pt2.c
+++ Ponteiros em C/Sobre Ponteiros pt2.c
@@ -1,13 +1,14 @@
+// Importando biblioteca de entrada e sa√≠da padr√£o
 #include <stdio.h>
 
 void incrementa(int* valor){
 	printf("Antes de incrementar:\n");
 	printf("O contador vale %d.\n", *valor);
-	printf("O endereÁo de memÛria È %d\n", valor);
+	printf("O endere√ßo de mem√≥ria √© %d\n", valor);
 
 	printf("Depois de incrementar:\n");
 	printf("O contador agora vale %d.\n", 2 + (*valor));
-	printf("O endereÁo de memÛria È %d\n", valor);
+	printf("O endere√ßo de mem√≥ria √© %d\n", valor);
 }
 
 int main(){
@@ -15,7 +16,7 @@ int main(){
 
 	incrementa(&contador);
 	printf("O contador agora vale %d\n", contador);
-	printf(" endereÁo de memÛria È %d\n", &contador);
+	printf(" endere√ßo de mem√≥ria √© %d\n", &contador);
 
 	return 0;
 }
