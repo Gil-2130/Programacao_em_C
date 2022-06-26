234d2075563ab95b567f70393c0952fd8ee0d771
Giliard P. Santos <gil2614@live.com>
2022-06-26 20:07:38 -0300

    Ponteiros Pt2

---
+++ Ponteiros em C/Sobre Ponteiros pt2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void incrementa(int* valor){
+	printf("Antes de incrementar:\n");
+	printf("O contador vale %d.\n", *valor);
+	printf("O endereço de memória é %d\n", valor);
+
+	printf("Depois de incrementar:\n");
+	printf("O contador agora vale %d.\n", 2 + (*valor));
+	printf("O endereço de memória é %d\n", valor);
+}
+
+int main(){
+	int contador = 10;
+
+	incrementa(&contador);
+	printf("O contador agora vale %d\n", contador);
+	printf(" endereço de memória é %d\n", &contador);
+
+	return 0;
+}
