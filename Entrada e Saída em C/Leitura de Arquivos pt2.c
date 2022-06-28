a23955a1a8ccbf06b0d4b5f73d8f96c9d5738f61
Giliard P. Santos <gil2614@live.com>
2022-06-28 12:53:19 -0300

    Add files via upload

---
+++ Entrada e Saída em C/Leitura de Arquivos pt2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+	FILE *arq;
+	char nome[10], *resultado;
+
+	arq = fopen("arquivo.txt", "r");
+
+	if (arq){
+		while (!feof(arq)){ // feof -  file End of File
+			resultado = fgets(nome, 10, arq);
+			printf("Resultado : %d\n", resultado);
+			if (resultado){
+				printf("%s\n", nome);
+			}
+		}
+	}
+	else {
+		printf("Não achei o arquivo");
+	}
+	fclose(arq);
+	return 0;
+}
