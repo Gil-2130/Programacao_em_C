6b4e348d0c8b29e47b7a4987638f40ee888d4976
Giliard P. Santos <gil2614@live.com>
2022-06-28 12:54:15 -0300

    Add files via upload

---
+++ Entrada e Saída em C/Leitura de Arquivos pt3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+	FILE *arq;
+	int num, resultado, soma = 0;
+
+	arq = fopen("arquivo0.txt", "r");
+
+	if (arq){
+		while (!feof(arq)){
+			resultado = fscanf(arq, "%d", &num);
+			printf("Resulado: %d\n", resultado);
+
+			if (resultado == 1){
+				soma = soma + num;
+			}
+		}
+	}
+	else {
+		printf("Não encontrei o arquivo.\n");
+	}
+	printf("A soma dos números encontrados é %d", soma);
+	fclose(arq);
+
+	return 0;
+}
