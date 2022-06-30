490c739f8e74b88e4387d14ad85d64cc0bc0e079
Giliard P. Santos <gil2614@live.com>
2022-06-30 12:35:15 -0300

    Definindo Tipos pt2

---
+++ Structs em C/Definindo Tipos pt2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
+struct st_aluno{
+	char nome[50];
+	char curso[100];
+	char matricula[10];
+	int ano_nasc;
+}aluno1;
+
+int main(){
+	printf("Informe o nome do aluno: \n");
+	fflush(stdout);
+	fgets(aluno1.nome, 50, stdin);
+
+	printf("Informe o curso desejado: \n");
+	fflush(stdout);
+	fgets(aluno1.curso,  100, stdin);
+
+	printf("Informe a matrícula do aluno: ");
+	fflush(stdout);
+	fgets(aluno1.matricula, 10, stdin);
+
+	printf("Informe o Ano de nascimento do aluno: \n");
+	fflush(stdout);
+	scanf("%d", &aluno1.ano_nasc);
+
+	printf("========= Dados do ALuno ========= \n");
+	printf("Nome: %s\n", aluno1.nome);
+	printf("Ano Nasc: %d\n", aluno1.ano_nasc);
+	printf("Curso: %s\n", aluno1.curso);
+	printf("Matrícula: %s\n", aluno1.matricula);
+
+	return 0;
+}
