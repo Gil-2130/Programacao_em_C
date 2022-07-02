b8843ed36bc9802fd8a4727c4c0d84d22bf084f9
Giliard P. Santos <gil2614@live.com>
2022-07-02 10:05:05 -0300

    IFdef

---
+++ Diretivas de Compilação em C/IFdef.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#ifndef PI
+	#define PI 3.14567
+#endif
+
+int main(){
+	// Variável
+	int valor = 5;
+
+	valor = 467;
+
+	printf("O valor é %d\n", valor);
+
+	printf("PI vale %f\n", PI);
+
+	//O código abaixo só será executado se a constante tiver sido definida
+#ifdef PI
+	printf("O valor de PI é %f\n", PI);
+#endif
+
+	return 0;
+}
+
