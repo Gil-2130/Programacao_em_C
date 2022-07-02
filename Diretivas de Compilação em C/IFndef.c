0aae35d6eab8c0089c5480c5c5ec06b8684a2e82
Giliard P. Santos <gil2614@live.com>
2022-07-02 10:04:37 -0300

    IFndef

---
+++ Diretivas de Compilação em C/IFndef.c
@@ -0,0 +1,28 @@
+/*
+ * IFndef - Diretiva de Compilação
+ */
+
+
+#include <stdio.h>
+
+// Criando Constante - Valor não pode ser alterado
+//#define PI 3.14567
+
+
+//Uso do IFndef (se determinada variável não tiver sido definida, então ela será criada
+#ifndef PI
+#define PI 3.14567
+#endif
+
+int main(){
+	// Criando Variável (valor pode ser alterado a qualquer momento durante a execução
+	int valor = 5;
+	printf("O valor é %d\n", valor);
+
+	valor = 450;
+
+	printf("Agora o valor é %d\n", valor);
+	printf("PI vale %.2f\n", PI * 2); // Podemos fazer alguma operação com o valor mas nuca alterá-lo
+
+	return 0;
+}
