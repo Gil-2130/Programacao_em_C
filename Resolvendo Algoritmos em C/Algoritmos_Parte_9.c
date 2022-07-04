55db920b8f652990e92d4aa2d40b4697b61d3806
Giliard P. Santos <gil2614@live.com>
2022-07-04 12:56:08 -0300

    Add files via upload

---
+++ Resolvendo Algoritmos em C/Algoritmos_Parte_9.c
@@ -0,0 +1,38 @@
+/*
+ * Ler um número e verificar se ele é par ou ímpar.
+ * Quando for par, armazenar esse valor em 'p'
+ * Quando for ímpar, armazenar este valor em 'i'.
+ * Exiba 'p' e 'i' no final do processamento.
+ *
+ * PSEUDOCÓDIGO
+ * p = 0
+ * i = 0
+ * receber valor
+ * se(valor % 2 == 0)
+ * 		então
+ * 		p = valor
+ * 	senão
+ * 		i = valor
+ * 	escrever p
+ * 	escrever i
+ */
+
+#include <stdio.h>
+
+int main(){
+	int num, p = 0, i = 0;
+
+	printf("Digite um valor: \n");
+	fflush(stdout);
+	scanf("%d", &num);
+
+	if (num % 2 == 0){
+		p = num;
+		printf("O número %d é par", num);
+	}
+	else if (num % 2 == 1){
+		i = num;
+		printf("O número %d é ímpar", num);
+	}
+	return 0;
+}
