7e56edfcfdca3d9e0c424d313c5630f9df9c21eb
Giliard P. Santos <gil2614@live.com>
2022-07-05 12:28:13 -0300

    Algoritmos Parte 1

--- Resolvendo Algoritmos em C/ALgoritmos_Parte_1.c
+++ Resolvendo Algoritmos em C/ALgoritmos_Parte_1.c
@@ -3,14 +3,14 @@
  *Faça um algoritmo para somar dois números e multiplicar
  *o resultado pelo primeiro número.
  *
- * Passos;
+ * Passos/Pseudocódigo;
  * 1 - Receba o primeiro numero via teclado
  * 2 - Receba o segundo numero via teclado
  * 3 - Realize a soma de ambos
  * 4 - Use o resultado da soma e multiplique pelo primeiro valor
  * 5 - Imprima o resultado
  * 6 - (opcional) mostre na tela de saída quais foram os números digitados e o resultado do processamento.
- 
+ *
  */
 
 // Importando Biblioteca de Entrada e Saída de dados
