c1120b702ccce2c8c7c8aeb3cb240ada0d692eb7
Giliard P. Santos <gil2614@live.com>
2022-07-08 13:09:00 -0300

    Algoritmos Parte 10

---
+++ Resolvendo Algoritmos em C/Algoritmos_Parte_010.c
@@ -0,0 +1,53 @@
+/*
+ * Tendo como dados de entrada; a altura e sexo de uma pessoa,
+ * construa um algoritmo que calcule seu peso ideal,
+ * utilizando as seguintes fórmulas:
+ * P/Homens; (72.7 * altura) - 58
+ * P/Mulheres; (62.1 * altura) - 44.7
+ *
+ * Receba altura
+ * Receba sexo
+ * se sexo == 'M' ou sexo == 'm' então
+ * 		peso_ideal = (72.7 * altura) - 58
+ * se sexo == 'F' ou sexo == 'f' então
+ *		peso_ideal = (62.1 * altura) - 44.7
+ * escrever peso_ideal
+ *
+ */
+
+#include <stdio.h>
+
+int main(){
+
+	float altura, peso_ideal;
+	char sexo;
+
+	printf("Informe a sua altura: ");
+	fflush(stdout);
+	scanf("%f", &altura);
+	getchar(); // ou gets(stdin) em ambos os casos funcionará
+
+	printf("Informe o seu sexo[M/F] ");
+	fflush(stdout);
+	scanf("%c", &sexo);
+
+	if (sexo == 'm' || sexo == 'M'){
+		peso_ideal = (72.7 * altura) - 58;
+		printf("Você é do sexo masculino, ");
+		printf("Seu peso ideal é %.2f", peso_ideal);
+	}
+	else if (sexo == 'f' || sexo == 'F'){
+		peso_ideal = (62.1 * altura) - 44.7;
+		printf("Você é do sexo feminino, ");
+		printf("Seu peso ideal é %.2f", peso_ideal);
+	}
+	else if (sexo != 'f' || sexo != 'F' || sexo != 'm' || sexo != 'M'){
+		printf("Opção inválida");
+	}
+	else{
+		printf("Opção Inválida!");
+	}
+
+
+	return 0;
+}
