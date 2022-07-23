4a069edafa27f6c2d541d7ebb0f53ba804db636b
Giliard P. Santos <gil2614@live.com>
2022-07-23 20:08:28 -0300

    Manipulação e criação de arquivos em C

--- Entrada e Saída em C/Manipulando Entradas e Saidas.c
+++ Entrada e Saída em C/Manipulando Entradas e Saidas.c
@@ -1,21 +1,22 @@
-// Importando biblioteca de entrada e saÌda padr„o
+// Importando biblioteca de entrada e sa√≠da padr√£o
 #include <stdio.h>
 
 // Programa principal
 int main(){
+	
 	// Criando arquivo no sistema
 	FILE *arq;
 
 	/* fopen(nome-do-arquivo, forma-de-abertura-do-arquivo)
 	 *
-	 * w = abrir o arquivo para escrita, se ele n„o existir ent„o ser· criado.
-	 * mas se o mesmo existir, ele ser· sobrescrito(muito cuidado ao usar este comando).
+	 * w = abrir o arquivo para escrita, se ele n√£o existir ent√£o ser√° criado.
+	 * mas se o mesmo existir, ele ser√° sobrescrito(muito cuidado ao usar este comando).
 	 *
-	 * r = abrir o arquivo apenas para leitura(n„o È possÌvel editar o arquivo)
+	 * r = abrir o arquivo apenas para leitura(n√£o √© poss√≠vel editar o arquivo)
 	 *
-	 * wa = abrir o arquivo para ediÁ„o de conte˙do(se o arquivo j· existir, o conte˙do ser· adicionado nas linhas abaixo)
+	 * wa = abrir o arquivo para edi√ß√£o de conte√∫do(se o arquivo j√° existir, o conte√∫do ser√° adicionado nas linhas abaixo)
 	 *
-	 * Sempre que finalizar a ediÁ„o, deve-se salvar e encerrar a conx„o do arquivo para n„o perder dados
+	 * Sempre que finalizar a edi√ß√£o, deve-se salvar e encerrar a conx√£o do arquivo para n√£o perder dados
 	 */
 
 	arq = fopen("arquivo.txt", "w");
