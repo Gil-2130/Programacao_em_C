b21f87ad0e07bfa7c83236e896ad13a8aa64e765
Giliard P. Santos <gil2614@live.com>
2022-07-24 20:58:46 -0300

    Usando Funções

--- Funcoes_em_C/Usando_Funcoes.c
+++ Funcoes_em_C/Usando_Funcoes.c
@@ -1,18 +1,18 @@
 /*
- * Usando funÁıes
+ * Usando fun√ß√µes
  *
- * main -> principal(ser· executado primeiro)
+ * main -> principal(ser√° executado primeiro)
  */
 
-// Importando biblioteca de entrada e saÌda padr„o
+// Importando biblioteca de entrada e sa√≠da padr√£o
 #include <stdio.h>
 
-// funÁ„o vazia (sem retorno)
+// fun√ß√£o vazia (sem retorno)
 void mensagem(){
-	printf("Ol· Mundo\n");
+	printf("Ol√° Mundo\n");
 }
 
-// FunÁ„o de soma
+// Fun√ß√£o de soma
 int soma(int num1, int num2){
 	return num1 + num2;
 	// ou
@@ -24,25 +24,23 @@ void proximo_char(char caractere){
 	printf("%c", caractere + 1);
 }
 
-//Programa principal(este ser· executado primeiro antes dos outros)
+//Programa principal(este ser√° executado primeiro antes dos outros)
 int main(){
 	// Imprimindo msg principal
-	printf("Ol·...\n");
+	printf("Ol√°...\n");
 
-
-	// FUN«√O MENSAGEM -> imprimindo a funÁ„o mensagem(void) criada anteriormente
+	// FUN√á√ÉO MENSAGEM -> imprimindo a fun√ß√£o mensagem(void) criada anteriormente
 	mensagem();
 
-
-	// FUN«√O SOMA -> Realizando c·lculo e imprimindo resultados
+	// FUN√á√ÉO SOMA -> Realizando c√°lculo e imprimindo resultados
 	int retorno = soma(4, 6);
-	printf("O retorno da operaÁ„o È %d\n", retorno);
+	printf("O retorno da opera√ß√£o √© %d\n", retorno);
 	// OU
 	printf("Retorno = %d\n", soma(5, 15));
 
 
 
-	// FUN«√O PROXIMO_CHAR -> Imprimindo caracteres
+	// FUN√á√ÉO PROXIMO_CHAR -> Imprimindo caracteres
 	char caractere = 'a';
 	proximo_char(caractere);
 
