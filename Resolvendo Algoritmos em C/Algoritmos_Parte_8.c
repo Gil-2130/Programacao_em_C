8bfb256187a806eed7549995070fe62db64a38f9
Giliard P. Santos <gil2614@live.com>
2022-07-25 22:55:45 -0300

    Programa "é par ou ímpar?"

--- Resolvendo Algoritmos em C/Algoritmos_Parte_8.c
+++ Resolvendo Algoritmos em C/Algoritmos_Parte_8.c
@@ -1,17 +1,17 @@
 /*
- * Elabore um algoritmo que leia um n˙mero.
+ * Elabore um algoritmo que leia um n√∫mero.
  * Se positivo, armazene-o em 'a'
  * Se negativo, armazene-o em 'b'
  * No final imprima o resultado
  *
- * PSEUDOC”DIGO;
+ * PSEUDOC√ìDIGO;
  *
  * recever valor
- * se valor > 0 ent„o;
+ * se valor > 0 ent√£o;
  * 		a = valor;
  * 		escrever "valor positivo";
  * 		escrever a
- * sen„o
+ * sen√£o
  * 		b = valor;
  * 		escrever "valor negativo";
  * 		escrever b
@@ -20,11 +20,12 @@
 #include <stdio.h>
 
 int main(){
-	// Declarando tipo e vari·vel
+	
+	// Declarando tipo e vari√°vel
 	int numero, a, b;
 
 	// Obtendo dados
-	printf("Digite um n˙mero: \n");
+	printf("Digite um n√∫mero: \n");
 	fflush(stdout);
 	scanf("%d", &numero);
 
@@ -38,7 +39,7 @@ int main(){
 		printf("Valor negativo: \n%d", b);
 	}
 	else{
-		printf("Valor %d n„o È positivo ou negativo", numero);
+		printf("Valor %d n√£o √© positivo ou negativo", numero);
 	}
 	return 0;
 }
