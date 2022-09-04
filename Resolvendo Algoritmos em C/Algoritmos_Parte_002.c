719d3e0313e355fc79071d1beeb1dda8e2839cb7
Giliard P. Santos <gil2614@live.com>
2022-09-03 22:00:09 -0300

    Update Algoritmos_Parte_002.c

--- Resolvendo Algoritmos em C/Algoritmos_Parte_002.c
+++ Resolvendo Algoritmos em C/Algoritmos_Parte_002.c
@@ -8,7 +8,7 @@
  * 	Receba qtd_minimo
  * 	Receba qtd_maximo
  * 	Some qtd_minimo + qtd_maximo
- * Divida o resultado da soma por 2
+ * 	Divida o resultado da soma por 2
  * Imprima o resultado da operação
 
 */
