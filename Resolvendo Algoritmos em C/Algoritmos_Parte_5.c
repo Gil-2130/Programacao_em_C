3f51a9a03cddda25fb1c90404df5ad351ac67027
Giliard P. Santos <gil2614@live.com>
2022-09-06 13:01:35 -0300

    Update Algoritmos_Parte_5.c

--- Resolvendo Algoritmos em C/Algoritmos_Parte_5.c
+++ Resolvendo Algoritmos em C/Algoritmos_Parte_5.c
@@ -7,7 +7,7 @@
  * 	A) Receba a quantidade de horas trabalhadas
  * 	B) Receba o valor da hora
  * 	C) Multiplique o número de horas trabalhadas pelo valor da hora
- * D) Mostre o resultado da operação
+ * 	D) Mostre o resultado da operação
  */
 
 #include <stdio.h>
