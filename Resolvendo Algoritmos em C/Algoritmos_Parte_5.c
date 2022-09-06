4576ab39dc693b1b83414d58c7d61d24c3711bfb
Giliard P. Santos <gil2614@live.com>
2022-09-06 13:01:15 -0300

    Update Algoritmos_Parte_5.c

--- Resolvendo Algoritmos em C/Algoritmos_Parte_5.c
+++ Resolvendo Algoritmos em C/Algoritmos_Parte_5.c
@@ -5,7 +5,7 @@
  *
  * CALCULA SALÁRIO/Passos (PSEUDOCÓDIGO)
  * 	A) Receba a quantidade de horas trabalhadas
- * B) Receba o valor da hora
+ * 	B) Receba o valor da hora
  * C) Multiplique o número de horas trabalhadas pelo valor da hora
  * D) Mostre o resultado da operação
  */
