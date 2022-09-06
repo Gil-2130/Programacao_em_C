d8d5446dae11980241568bc3cbe0ad8aca45a4f6
Giliard P. Santos <gil2614@live.com>
2022-09-06 13:01:05 -0300

    Update Algoritmos_Parte_5.c

--- Resolvendo Algoritmos em C/Algoritmos_Parte_5.c
+++ Resolvendo Algoritmos em C/Algoritmos_Parte_5.c
@@ -4,7 +4,7 @@
  * Calcule e mostre o total do seu salário no referido mês.
  *
  * CALCULA SALÁRIO/Passos (PSEUDOCÓDIGO)
- * A) Receba a quantidade de horas trabalhadas
+ * 	A) Receba a quantidade de horas trabalhadas
  * B) Receba o valor da hora
  * C) Multiplique o número de horas trabalhadas pelo valor da hora
  * D) Mostre o resultado da operação
